83da21cc53abadebceb97bd519a223f12feb5157
saujanyamahajan <saujanya.9919@gmail.com>
2026-01-08 10:00:00 +0530

    Second Largest

---
+++ SORTING/SelectionSort.cs
@@ -0,0 +1,31 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int[] nums = { 64, 25, 12, 22, 11 };
+        SelectionSort(nums);
+        Console.WriteLine(string.Join(" ", nums));   
+    }
+    public  static void SelectionSort (int[] nums)
+    {
+        int n = nums.Length;
+        for( int i=0;i<n-1;i++)
+        {
+            int min=i;
+            for(int j =i+1;j<n;j++ )
+            {
+                if(nums[j]<nums[min])
+                min=j;
+            }
+            int temp=nums[min];
+            nums[min]=nums[i];
+            nums[i]=temp;
+            
+        }
+    }
+}
