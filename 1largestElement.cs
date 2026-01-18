ae4c969cdd38000d5b3b8fa420ecd6c5b2367eb4
saujanyamahajan <saujanya.9919@gmail.com>
2026-01-18 12:21:38 +0530

    largestElement

---
+++ 1largestElement.cs
@@ -0,0 +1,14 @@
+﻿public class Solution {
+    public int largestElement(int[] nums) {
+        
+        int max = int.MinValue;
+        foreach(int i in nums) 
+        {
+            if (i>max)
+            {
+                max=i;
+            }
+        }
+        return max;
+    }
+}
