05f5c694280180161d44e412b14e50104b2d40d2
saujanyamahajan <saujanya.9919@gmail.com>
2026-02-18 10:00:00 +0530

    Quick Sort

---
+++ SORTING/QuickSort.cs
@@ -0,0 +1,31 @@
+public static void QuickSort (int[] arr,int low,int high)
+{
+    if(low<high)
+    {
+        int pivot = partition (arr,low,high);
+
+        QuickSort(arr,low,pivot-1);
+        QuickSort(arr,pivot+1,high);
+    }
+}
+private static int partition(int[]arr, int low,int high)
+{
+    int pivot=arr[high];
+    int i=low-1;
+    for(int j= low;j<high;j++)
+    {
+        if(arr[j]<pivot)
+        {
+            i++;
+            swap(arr,i,j);
+        }
+    }
+    swap(arr,i+1,high);
+    return i+1;
+}
+private static void swap(int[] arr, int i,int j)
+{
+    int temp=arr[i];
+    arr[i]=arr[j];
+    arr[j]=temp;
+}
