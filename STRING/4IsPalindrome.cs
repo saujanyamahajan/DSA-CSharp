95d4c7cf1186bb6a530feefe780509a66f7c3508
saujanyamahajan <saujanya.9919@gmail.com>
2026-02-25 21:57:54 +0530

    IsPalindrome

--- STRING/4IsPalindrome.cs
+++ STRING/4IsPalindrome.cs
@@ -17,3 +17,30 @@ public class HelloWorld
             Console.WriteLine("False");
     }
 }
+
+///
+
+public class Solution {
+    public bool IsPalindrome(string s) {
+        int left =0;
+        int right=s.Length-1;
+
+        while(left<right)
+        {
+        while(left<right && !char.IsLetterOrDigit(s[left]))
+        {
+            left++;
+        }
+        while(left<right && !char.IsLetterOrDigit(s[right]))
+        {
+            right--;
+        }
+         if (char.ToLower(s[left]) != char.ToLower(s[right]))
+                return false;
+
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
