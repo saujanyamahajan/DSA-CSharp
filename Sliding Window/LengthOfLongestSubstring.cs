6a2f06bf25569ea3163f569d27687ed944eb56ca
saujanyamahajan <saujanya.9919@gmail.com>
2026-03-05 23:55:37 +0530

    LengthOfLongestSubstring

---
+++ Sliding Window/LengthOfLongestSubstring.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        HashSet<char>set=new HashSet<char>();
+        int left=0;
+        int maxLength=0;
+        for(int right=0;right<s.Length;right++)
+        {
+            while(set.Contains(s[right]))
+            {
+                set.Remove(s[left]);
+                left++;
+            }
+            set.Add(s[right]);
+            maxLength=Math.Max(maxLength,right-left+1);
+        }
+        return maxLength;
+    }
+}
