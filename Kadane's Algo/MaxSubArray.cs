20ab5a03c45cbcc2b6c6f6adb6d2337ce4c5432b
saujanyamahajan <saujanya.9919@gmail.com>
2026-03-17 10:00:00 +0530

    MaxSubArray

---
+++ Kadane's Algo/MaxSubArray.cs
@@ -0,0 +1,15 @@
+// Find the maximum sum of a contiguous subarray
+
+public int MaxSubArray(int[] nums)
+{
+    int currentSum = nums[0];
+    int maxSum = nums[0];
+
+    for (int i = 1; i < nums.Length; i++)
+    {
+        currentSum = Math.Max(nums[i], currentSum + nums[i]);
+        maxSum = Math.Max(maxSum, currentSum);
+    }
+
+    return maxSum;
+}
