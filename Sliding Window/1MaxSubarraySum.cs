f3b0fa2370b4d862258ad5f0997de2a2743a2b66
saujanyamahajan <saujanya.9919@gmail.com>
2026-03-27 10:00:00 +0530

    max sum of sub array

--- Sliding Window/1MaxSubarraySum.cs
+++ Sliding Window/1MaxSubarraySum.cs
@@ -1,17 +1,29 @@
 class Solution {
     public int maxSubarraySum(int[] nums, int k) {
-        // Code here
         int maxSum=0;
         int windowSum=0;
+            // First window
         for(int i=0;i<k;i++)
-            windowSum=windowSum+nums[i];
-            maxSum=windowSum;
-        for(int i=k;i<nums.Length;i++)
         {
             windowSum=windowSum+nums[i];
-            windowSum=windowSum-nums[i-k];
+        }
+            maxSum=windowSum;
+                // Slide the window
+        for(int i=k;i<nums.Length;i++)
+        {
+            windowSum=windowSum+nums[i]; // add next
+            windowSum=windowSum-nums[i-k]; // remove first
             maxSum=Math.Max(windowSum,maxSum);
         }
         return maxSum;
     }
 }
+
+// Idea:
+
+// Compute sum of first window of size k
+// Then slide window:
+// Add next element
+// Remove previous element
+
+// Fixed window size → Sliding Window
