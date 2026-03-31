d8e8c7ec3eadec278066ee64b75ef8fcf7a3e1f6
saujanyamahajan <saujanya.9919@gmail.com>
2026-03-31 10:00:00 +0530

    lastOccurrenceBinary

--- BINARY SEARCH/lastOccurrence.cs
+++ BINARY SEARCH/lastOccurrence.cs
@@ -9,3 +9,28 @@ function lastOccurrence(arr, target) {
 }
 
 
+
+//Binary Search
+function lastOccurrenceBinary(arr, target) 
+{
+    int result = -1;
+    int left=0;
+    int right=arr.Length-1;
+     
+     while(left<=right)
+     {
+        int mid=Math.floor((left+right)/2);
+        if(arr[mid]==target)
+            result= mid;
+            left=mid+1;
+        elseif (arr[mid]<target)
+            left=mid+1;
+        else
+            right=mid-1;
+     }
+    return result;
+}
+
+
+// Unsorted → Linear Search (O(n))
+// Sorted → Binary Search (O(log n))
