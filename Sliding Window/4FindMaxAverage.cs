276f7595180218c5a096bf352ea2189d67b5a14c
saujanyamahajan <saujanya.9919@gmail.com>
2026-04-06 17:39:59 +0530

    FindMaxAverage approch

---
+++ Sliding Window/4FindMaxAverage.cs
@@ -0,0 +1,9 @@
+// This is a fixed sliding window problem 🔥
+// 👉 Find the maximum average of any subarray of size k
+
+// ✅ Key Idea
+// Average = sum / k
+// So instead of recomputing every time:
+// 👉 Find max sum of subarray of size k, then divide by k
+
+// ✅ Optimal Approach: Sliding Window (O(n))
