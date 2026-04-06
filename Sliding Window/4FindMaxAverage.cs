071a1a25803b7ff76e12ea2ee31aa8e958b29aed
saujanyamahajan <saujanya.9919@gmail.com>
2026-04-06 17:40:20 +0530

    FindMaxAverage

--- Sliding Window/4FindMaxAverage.cs
+++ Sliding Window/4FindMaxAverage.cs
@@ -7,3 +7,26 @@
 // 👉 Find max sum of subarray of size k, then divide by k
 
 // ✅ Optimal Approach: Sliding Window (O(n))
+
+
+double FindMaxAverage(int[] arr, int k)
+{
+ int windowSum=0;
+ int maxSum=0;
+ //first window
+ for(int i=0;i<k;i++)
+ {
+    windowSum+=arr[i];
+ }
+ int maxSum = windowSum;
+
+ //slide window
+ for(int i=k;i<arr.Length;i++)
+ {
+    windowSum+=arr[i];
+    windowSum+=arr[i-k];
+    maxSum=Math.Max(windowSum,maxSum);
+ }
+ return (double)maxSum/k;
+
+}
